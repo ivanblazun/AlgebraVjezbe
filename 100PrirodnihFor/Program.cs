021e864b598bfe1cd5db19460b606ccb64d7e2bc
Ivan Blazun <87031660+ivanblazun@users.noreply.github.com>
2023-07-03 23:35:34 +0200

    prirodniLoop

---
+++ 100PrirodnihFor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100PrirodnihFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100 ; i++) 
+            {
+                Console.WriteLine("{0},00",i +1 );
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
