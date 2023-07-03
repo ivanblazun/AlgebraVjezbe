6175221452d5b4c4f41472fa772c024ddc1da965
Ivan Blazun <87031660+ivanblazun@users.noreply.github.com>
2023-07-03 21:03:13 +0200

    piramida

---
+++ Nizovi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nizovi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string recenice = "Danas je lijep dan";
+            //char[] slova = new char[recenice.Length];
+
+            //for (int i = 0; i < recenice.Length; i++)
+            //{   
+                
+            //    slova[i] = recenice[i];
+            //    bool isEmpty = slova[i] == ' ';
+            //    if (isEmpty)
+            //    {
+            //        continue;
+            //    }
+            //    if (slova[i] == 'p') {
+            //        break;
+            //    }
+            //    Console.WriteLine(slova[i]);
+            //}
+
+            //List<int> list = new List<int>();
+
+            //for(var i = 0; i <= 20; i++)
+            //{
+            //    list.Add(i);
+            //}
+
+            //for(var i = 0;i <= 20; i++)
+            //{
+            //    Console.WriteLine(list[i]);
+            //}
+            
+            int counter = 0;
+            do { counter++; Console.WriteLine(counter); } while (counter < 10);
+            Console.WriteLine("\n");
+            counter++;
+            do {counter--; Console.WriteLine(counter);} while (counter >1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PiramidaFor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiramidaFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int broj = 10;
+            int razmak = broj;
+
+            for (int i = 0; i <= broj; i++)
+            {
+                razmak--;
+
+                for (int j = 0; j <= razmak; j++)
+                {
+                    Console.Write(" ");
+
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+
+                    Console.Write("*");
+                }
+
+                for (int x = 0; x <= i; x++)
+                {
+
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
