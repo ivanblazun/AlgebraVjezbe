2434c2d5bb4ed8365ec65b69147fd124907c42e7
Ivan Blažun <ivan.blazun@dignet.hr>
2023-07-06 12:34:25 +0200

    array work

--- IspisNeparnihBrojeva/Program.cs
+++ IspisNeparnihBrojeva/Program.cs
@@ -10,13 +10,14 @@ namespace IspisNeparnihBrojeva
     {
         static void Main(string[] args)
         {
-            int[,] nizBrojeviBrojac = new int[2,1];
+            int[,] nizBrojeviBrojac = new int[2,2];
             
             Console.WriteLine("Unesite interval brojeva i dobi cete ispisane neparne brojeve...");
 
             int pocetniUnos = UnosBroja(nizBrojeviBrojac);
+            Console.WriteLine(pocetniUnos);
             int zadnjiUnos = UnosBroja(nizBrojeviBrojac);
-
+            Console.WriteLine();
             //IspisIntervala(brojevi);
 
             Console.ReadLine();
@@ -25,11 +26,9 @@ namespace IspisNeparnihBrojeva
         }
         private static int UnosBroja(int[,] nizBrojeviBrojac)
         {
-            nizBrojeviBrojac[0, nizBrojeviBrojac[1, 0] - 1] = Convert.ToInt32(Console.ReadLine());
-            nizBrojeviBrojac[1, 0] += 1;
-            Console.WriteLine(nizBrojeviBrojac[0, nizBrojeviBrojac[1, 0 ] - 1]);
+            nizBrojeviBrojac[1, 0]++;
+            nizBrojeviBrojac[0, nizBrojeviBrojac[1, 0]-1] = Convert.ToInt32(Console.ReadLine());
             return nizBrojeviBrojac[0, nizBrojeviBrojac[1, 0]-1];
-
 
         }
 
